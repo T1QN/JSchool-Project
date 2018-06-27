e00d32d37591ffa3373b8e781efe7540c33b12bc
Vormeh <DimaTihon2Gmail.com>
2018-06-27 11:17:26 +0300

    First including Hibernate context.

---
+++ src/main/java/com/model/hibernate/car/Car.java
@@ -0,0 +1,7 @@
+package com.model.hibernate.car;
+
+/**
+ * Created by Mandrake on 25.06.2018.
+ */
+public class Car {
+}
---
+++ src/main/java/com/model/hibernate/user/dao/UserTypeDao.java
@@ -0,0 +1,7 @@
+package com.model.hibernate.user.dao;
+
+/**
+ * Created by Mandrake on 25.06.2018.
+ */
+public class UserTypeDao {
+}
